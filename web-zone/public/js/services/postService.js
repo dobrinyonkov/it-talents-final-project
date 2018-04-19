c4c5e8a1e11833856042e6a50c66727c685b2c56
Radostin Dimitrov <dimitrov.radostin@abv.bg>
2018-04-19 16:19:53 +0300

    syncronization

--- web-zone/public/js/services/postService.js
+++ web-zone/public/js/services/postService.js
@@ -1,45 +1,75 @@
-var postServices = () => {
-
-    function Post(ownerId, text, photo) {
-        this.text = text;
-        this.ownerId = owner;
-        // this.photo=???tva url li e
-        this.date = new Date();
-        this.comments = []
-        this.likes = []//masiv ot idta na tiq det sa go haresali 
-
+var postService = (() => {
+  function Post(ownerId, text, photo) {
+    this.text = text;
+    this.ownerId = ownerId;
+    if (photo) {
+      this.photo = photo;
     }
-    function Comment(ownerId, text) {
-        this.ownerId = ownerKId
-        this.text = text
-        this.date = new Date()
-    }
+    this.date = new Date();
+    this.comments = [{ ownerId:42,text:"nqkav komentar",date:"dneska v 6 bez 10" }];
+    this.likes = []
+  }
+  function Comment(ownerId, text) {
+    this.ownerId = ownerId;
+    this.text = text;
+    this.date = new Date();
+  }
+  //tva se vika postServices.getPost(id).then(p=>p.addComment(id,text))
+  Post.prototype.addComment = function(ownerId, text) {
+    // this.comments.push(new Comment(ownerId, text));
+    // var newPost = this;
+    // $.ajax({
+    //   type: "PUT",
+    //   url: "/api/posts",
+    //   contentType: "application/json",
+    //   data: JSON.stringify(newPost)
+    // }).done(res => {
+    //   console.log("put zaqvkata varna neshto ");
+    //   console.log(res);
+    // });
+    //   return $.post("/api/posts", post).done(response => {
+    //     console.log(response);
+    //     return response;
+    //   });
+    //   var newC=new Comment(ownerId ,text)
+    //   //var post=find that post from db
+    //   post.comments.push(newC)
+    //   //db.posts.save(post)
+  };
+  Post.prototype.deleteComment = function(id) {};
 
-    Post.prototype.addComment = function (postId, ownerId, text) {
-        var newC = new Comment(ownerId, text)
-        //var post=find that post from db
-        post.comments.push(newC)
-        //db.posts.save(post)
-    }
+  var result = {};
 
 
-    var result = {}
-    //dali v toz method sa zapazvat vav bazata ili oshte vav Post constructora ,che da moje da im vzemem id-to
-    result.addPost = function (owner, text, photo) {
-        var newP = new Post(owner, text, photo)
+  //ei tva moje da go npravim da raboti kato factory
+  result.addPost = function(ownerId, text, photo) {
+    var newP = new Post(ownerId, text, photo);
+    console.log(newP);
+    newP = JSON.parse(JSON.stringify(newP));
+    // newR=JSON.stringify(newP)
+    return $.post("/api/posts", newP,"application/json").done(response => {
+      console.log(response);
+      return response;
+    });
+  };
 
-        //da go nahaka v bazata
-        //db.post.insert(JSON.stringyfy(newP))
+  result.deletePost = function(id) {};
 
-        //da varne id-to
-        return //db.posts.find({owner:owner,text:text,photo:photo})._id
-    }
-    result.getPost = function (id) {
+  result.getPost = function(id) {
+    //teq kato gi returnvam napravo posle getPost e promise
+    return $.get("/api/posts/" + id).then(postData => {
+      // console.log("i sq tva api kvo vrashta")
+      // console.log(postData)
+      var post = Object.create(Post.prototype);
+      for (const key in postData) {
+        if (postData.hasOwnProperty(key)) {
+          post[key] = postData[key];
+        }
+      }
+      console.log(post);
+      return post;
+    });
+  };
 
-        //var post=db.post.find({_id=id})
-        // vse edno post.__proto__=Post.prototype , maj behse s Object.create
-        return post
-    }
-
-    return result
-};
+  return result;
+})();
