d21878a11aa32bb4412e55402e73878567057df6
Dobrin Yonkov <dobrin.yonkov1@gmail.com>
2018-04-26 18:13:45 +0300

    merged

--- web-zone/public/js/post/postController.js
+++ web-zone/public/js/post/postController.js
@@ -1,15 +1,12 @@
 app.controller("postController", function($scope, postService) {
-  $scope.greeting2 = "hello from the post controller";
-  $scope.greeting1 = postService.greeting1;
 
 
   postService.getPost("5ad89f57ee0c5a17321e044a").then(r => {
-    console.log("taz zaqvka se sluchi");
+    // console.log("taz zaqvka se sluchi");
     $scope.postInfo = r;
   });
 
-
   $scope.postPost=postService.addPost
-  $scope.testAddingAComment=postService.addComment
+  // $scope.testAddingAComment=postService.addComment
 
 });
