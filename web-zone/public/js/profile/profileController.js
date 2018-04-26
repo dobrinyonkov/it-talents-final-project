a2ac138dbbe73e9a16cd5863d2aedeb6feca8c26
Radostin Dimitrov <dimitrov.radostin@abv.bg>
2018-04-26 11:18:53 +0300

    pull

--- web-zone/public/js/profile/profileController.js
+++ web-zone/public/js/profile/profileController.js
@@ -1,3 +1,23 @@
+function calculateTimeInterval(date){
+    var interval=Date.now()-Date.parse(date)
+    // console.log(interval)
+    
+    if(interval<5000)return "just now";
+    interval=Math.floor(interval/1000)
+    if(interval<60)return interval+" seconds ago";
+    interval=Math.floor(interval/60);
+    if(interval<2)return "1 minute ago";
+    if(interval<60)return interval+" minutes ago";
+    interval=Math.floor(interval/60);
+    if(interval<2)return "1 hour ago";
+    if(interval<24)return interval+" hours ago";
+    interval=Math.floor(interval/24);
+    if(interval<2)return "yesterday";
+    return interval+" days ago"
+}
+// console.log(new Date)
+// console.log(calculateTimeInterval("2018-04-25T21:47:01.544Z"))
+
 (function () {
     app.controller('ProfileController', function ($scope, postService) {
         // $scope.currentUser=
