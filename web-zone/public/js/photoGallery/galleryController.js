728e2707ecd34b6268b7744bd89139b458a00280
Radostin Dimitrov <dimitrov.radostin@abv.bg>
2018-05-05 00:37:56 +0300

    Photo gallery.

--- web-zone/public/js/photoGallery/galleryController.js
+++ web-zone/public/js/photoGallery/galleryController.js
@@ -1,19 +1,40 @@
-(function () {
-  app.controller('PhotoGalleryController', function (
+(function() {
+  app.controller("PhotoGalleryController", function(
     // $window,
     // $rootScope,
     // fileUpload,
     $routeParams,
     $scope,
+    // $apply,
     // PostService,
-    // UserService,
-    // $timeout
+    UserService,
+    $timeout
   ) {
-    console.log("toz kontroller bachka")
+    $scope.displayedPhoto = "";
+    $scope.changePhoto = function(photo) {
+      $scope.displayedPhoto = photo;
+    };
+    $scope.next=function(){
+        var index =$scope.profile.photos.indexOf($scope.displayedPhoto)
+        index++
+        if(index>=$scope.profile.photos.length){
+            index=0
+        }
+        $scope.displayedPhoto=$scope.profile.photos[index]
+    }
+    $scope.previous=function(){
+        var index =$scope.profile.photos.indexOf($scope.displayedPhoto)
+        index--
+        if(index<0){
+            index=$scope.profile.photos.length-1
+        }
+        $scope.displayedPhoto=$scope.profile.photos[index]
+    }
+
     var userId = $routeParams.id;
-    console.log(userId);
-    // UserService.getAndSafeLoggedUser(userId).then(r => {
-    //   $scope.profile = r;
-    // });
+    UserService.getById(userId).then(r => {
+      console.log(r);
+      $scope.profile = r.data[0];
+    });
   });
 })();
